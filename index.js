79fca5102b45b8ba6a1d881d86252d28f553438f
Ronei Costa Soares <ronei0612@gmail.com>
2024-09-30 15:08:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; 
 
 app.get('/fetch-url', async (req, res) => {
   const { url } = req.query;
