a8539811510a64e9b81e9e064ad06a86ff89c481
Ronei Costa Soares <ronei0612@gmail.com>
2024-09-30 14:58:54 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+
+app.get('/fetch-url', async (req, res) => {
+  const { url } = req.query;
+
+  if (!url) {
+    return res.status(400).send('URL is required');
+  }
+
+  try {
+    const response = await axios.get(url);
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).send('Error fetching the URL');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`API listening at http://localhost:${port}`);
+});
