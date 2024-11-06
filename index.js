676b3623c96c007d9aad25400ae6af0b65a3daa2
Ronei Costa Soares <ronei.costa@aplicativo.net>
2024-11-06 10:33:36 -0300

    feat(api): Implementar rota /downloadsite

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const cheerio = require('cheerio');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
 
 app.get('/fetch-url', async (req, res) => {
   const { url } = req.query;
@@ -30,6 +32,58 @@ app.get('/fetch-url', async (req, res) => {
     res.status(500).send('Error fetching the URL');
   }
 });
+
+app.post('/downloadsite', async (req, res) => {
+  const { url } = req.body;
+
+  if (!url) {
+    return res.status(400).json({ error: 'URL is required' });
+  }
+
+  try {
+    const response = await axios.get(url);
+    const html = response.data;
+
+    const { tom, cifraTexto } = obterCifra(html);
+
+    res.json({ tom, cifraTexto });
+
+  } catch (error) {
+    console.error('Erro ao buscar a URL:', error);
+    res.status(500).json({ error: 'Error fetching the URL' });
+  }
+});
+
+function obterCifra(html) {
+  const $ = cheerio.load(html);
+
+  let tom = '';
+  const tomElement = $('#cifra_tom a');
+  if (tomElement.length > 0) {
+    //tom = tomElement.attr('title').split('>')[1].split('<')[0].trim();
+    tom = tomElement.text().trim();
+  }
+
+  $('span.tablatura').remove();
+  
+  let cifraTexto = $('pre').text().trim();
+  if (cifraTexto === '') {
+    return { tom, cifraTexto: 'Cifra não encontrada' };
+  }
+
+  if (!cifraTexto.startsWith('<pre>')) {
+    cifraTexto = '<pre>' + cifraTexto;
+  }
+  if (!cifraTexto.endsWith('</pre>')) {
+    cifraTexto += '</pre>';
+  }
+  if (tom === '') {
+    tom = 'C';
+    cifraTexto += '\n\n<p>Tom não encontrado.</p>';
+  }
+
+  return { tom, cifraTexto };
+}
 
 app.get('/', async (req, res) => {
   res.send('Ok');
